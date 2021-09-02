15e8b85387e804b8dac50e06f2454ae0d38eb2ec
johannah <jh1736@gmail.com>
2021-09-02 14:49:52 -0400

    added nearfrontview camera and randomize_on_init to robosuite wrapper

--- robosuite/demos/demo_domain_randomization.py
+++ robosuite/demos/demo_domain_randomization.py
@@ -5,7 +5,8 @@ Script to showcase domain randomization functionality.
 import robosuite.utils.macros as macros
 from robosuite.controllers import load_controller_config
 from robosuite.utils.input_utils import *
-from robosuite.wrappers import DomainRandomizationWrapper
+from robosuite.wrappers import DomainRandomizationWrapper, GymImageDomainRandomizationWrapper
+
 from robosuite.wrappers.domain_randomization_wrapper import DEFAULT_DYNAMICS_ARGS, DEFAULT_LIGHTING_ARGS, DEFAULT_CAMERA_ARGS, DEFAULT_COLOR_ARGS
 from IPython import embed; 
 
@@ -68,27 +69,26 @@ if __name__ == "__main__":
     dynamics_args = DEFAULT_DYNAMICS_ARGS
     camera_args = DEFAULT_CAMERA_ARGS
     lighting_args = DEFAULT_LIGHTING_ARGS
-    env = DomainRandomizationWrapper(env, 
-            randomize_dynamics=True, 
-            dynamics_randomization_args=dynamics_args,
-            randomize_camera=True, 
-            camera_randomization_args=camera_args,
-            randomize_color=True, 
-            color_randomization_args=color_args,
-            randomize_lighting=True, 
-            lighting_randomization_args=lighting_args,
-            randomize_on_reset=True, 
-            randomize_every_n_steps=0)
-    env.reset()
 
     # Get action limits
     low, high = env.action_spec
-
-    # do visualization
-    for e in range(100):
-        for i in range(40):
-            action = np.random.uniform(low, high)
-            obs, reward, done, _ = env.step(action)
-            env.render()
+    for re in range(20):
+        env = GymImageDomainRandomizationWrapper(env, 
+                randomize_dynamics=True, 
+                dynamics_randomization_args=dynamics_args,
+                randomize_camera=True, 
+                camera_randomization_args=camera_args,
+                randomize_color=True, 
+                color_randomization_args=color_args,
+                randomize_lighting=True, 
+                lighting_randomization_args=lighting_args,
+                randomize_on_reset=False, 
+                randomize_every_n_steps=0)
         env.reset()
+        for e in range(5):
+            for i in range(40):
+                action = np.random.uniform(low, high)
+                obs, reward, done, _ = env.step(action)
+                env.render()
+            env.reset()
         #env.modders[0].change_default_texture('table_visual', np.random.randint(len(env.modders[0].textures)))
